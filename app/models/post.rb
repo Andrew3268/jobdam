431fccb777cc49ad1d71e8009d2c9927a35693db
이정훈 <junghoonlee@192-168-1-17.tpgi.com.au>
2018-03-05 00:01:56 +1000

    Jobdam First Commit

---
+++ app/models/post.rb
@@ -0,0 +1,10 @@
+class Post < ApplicationRecord
+
+	acts_as_votable
+	
+	belongs_to :user
+	has_many :comments
+
+	has_attached_file :image, styles: { medium: "700x450#", small: "350x250>" }
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+end
